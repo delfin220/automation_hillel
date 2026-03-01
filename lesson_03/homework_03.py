82aa6142bc0f7f4d3aa42825ccba41a45a1fec48
delfin220 <ya.terzeman@gmail.com>
2026-03-01 15:29:44 +0200

    added lesson 3

---
+++ lesson_03/homework_03.py
@@ -0,0 +1,143 @@
+# task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
+alice_in_wonderland = (
+    '"Would you tell me, please, which way I ought to go from here?"\n'
+    '"That depends a good deal on where you want to get to," said the Cat.\n'
+    '"I don\'t much care where ——" said Alice.\n'
+    '"Then it doesn\'t matter which way you go," said the Cat.\n'
+    '"—— so long as I get somewhere," Alice added as an explanation.\n'
+    '"Oh, you\'re sure to do that," said the Cat, "if you only walk long enough."'
+)
+
+# task 02 == Знайдіть та відобразіть всі символи одинарної лапки (') у тексті
+for number, lapka in enumerate(alice_in_wonderland):
+    if lapka == "'":
+        print(f"Символ ' знайдено на позиції {number}")
+# task 03 == Виведіть змінну alice_in_wonderland на друк
+
+
+print(alice_in_wonderland)
+print()
+
+
+"""
+    # Задачі 04 -10:
+    # Переведіть задачі з книги "Математика, 5 клас"
+    # на мову пітон і виведіть відповідь, так, щоб було
+    # зрозуміло дитині, що навчається в п'ятому класі
+"""
+# task 04
+"""
+Площа Чорного моря становить 436 402 км2, а площа Азовського
+моря становить 37 800 км2. Яку площу займають Чорне та Азов-
+ське моря разом?
+"""
+
+black_sea = 436402
+azov_sea = 37800
+total = black_sea + azov_sea
+print("Площа Азовсткого та Чорного морів разом займає: ", total, "км квадратних")
+print()
+
+
+
+
+# task 05
+"""
+Мережа супермаркетів має 3 склади, де всього розміщено
+375 291 товар. На першому та другому складах перебуває
+250 449 товарів. На другому та третьому – 222 950 товарів.
+Знайдіть кількість товарів, що розміщені на кожному складі.
+"""
+
+total_warehouse = 375291
+first_warehouse = total_warehouse - 222950
+third_warehouse = total_warehouse - 250449
+second_warehouse = total_warehouse - (first_warehouse + third_warehouse)
+
+
+print(f"На першому складі: {first_warehouse}, товарів\nНа другому складі: {second_warehouse} товарів\nНа третьому складі: {third_warehouse} товарів")
+print()
+
+# task 06
+"""
+Михайло разом з батьками вирішили купити комп’ютер, ско-
+риставшись послугою «Оплата частинами». Відомо, що сплачу-
+вати необхідно буде півтора року по 1179 грн/місяць. Обчисліть
+вартість комп’ютера.
+"""
+month_payment = 1179
+credit_time_moths = 18
+computer_price = credit_time_moths * month_payment
+
+print(f"Ціна компьютера: {computer_price} uah")
+print()
+
+# task 07
+"""
+Знайди остачу від діленя чисел:
+a) 8019 : 8     d) 7248 : 6
+b) 9907 : 9     e) 7128 : 5
+c) 2789 : 5     f) 19224 : 9
+"""
+print(f"a) 8019 : 8 = остача {8019 % 8}\nb) 9907 : 9 = остача {9907 % 9}\nc) 2789 : 5 = остача {2789 % 5}\nd) 7248 : 6 = остача {7248 % 6}\ne) 7128 : 5 = остача {7128 % 5}\nf) 19224 : 9 = остача {19224 % 9}")
+print()
+
+
+
+
+# task 08
+"""
+Іринка, готуючись до свого дня народження, склала список того,
+що їй потрібно замовити. Обчисліть, скільки грошей знадобиться
+для даного її замовлення.
+Назва товару    Кількість   Ціна
+Піца велика     4           274 грн
+Піца середня    2           218 грн
+Сік             4           35 грн
+Торт            1           350 грн
+Вода            3           21 грн
+"""
+
+pizza_big = 274 * 4
+pizza_average = 218 * 2
+juice = 4 * 35
+cake = 350
+water = 3 * 21
+product_total = pizza_big + pizza_average + juice + cake + water
+print("Грошей знадобиться", product_total)
+print()
+
+
+# task 09
+"""
+Ігор займається фотографією. Він вирішив зібрати всі свої 232
+фотографії та вклеїти в альбом. На одній сторінці може бути
+розміщено щонайбільше 8 фото. Скільки сторінок знадобиться
+Ігорю, щоб вклеїти всі фото?
+"""
+total_photos = 232
+photos_in_page = 8
+total_pages = 232 // 8
+print("Сторінок знадобиться", total_pages)
+
+# task 10
+"""
+Родина зібралася в автомобільну подорож із Харкова в Буда-
+пешт. Відстань між цими містами становить 1600 км. Відомо,
+що на кожні 100 км необхідно 9 літрів бензину. Місткість баку
+становить 48 літрів.
+1) Скільки літрів бензину знадобиться для такої подорожі?
+2) Скільки щонайменше разів родині необхідно заїхати на зап-
+равку під час цієї подорожі, кожного разу заправляючи пов-
+ний бак?
+"""
+
+distance_km = 1600
+tank_capacity = 48
+fuel_per_100km = 9
+
+fuel_for_trip = fuel_per_100km * (distance_km / 100)
+print(f"Літрів бензину знадобиться: ", fuel_for_trip)
+number_of_refuels = fuel_for_trip // tank_capacity
+print("Родині необхідно заїхати на заправку під час цієї подорожі", number_of_refuels)
+
